48593479b0199ac77b1b59cbf712af8a6676387e
alessandrasciandt <alessandras@ciandt.com>
2022-08-18 12:32:19 -0400

    0. Serviço Calculador de Salário

---
+++ 0x01/salario/SalarioService.java
@@ -0,0 +1,15 @@
+import java.util.UUID;
+
+public class SalarioService {
+
+    private CalculadorSalarioService calculador = CalculadorSalarioService.getCalculadorSalarioService();
+
+    public double calcular(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
+        double resultado = CalculadorSalarioService.instancia.calcularSalarioLiquido(salarioBruto, valorDescontos, valorVendas, percentualComissao);
+        return resultado;
+    }
+
+    public UUID getUuid(){
+        return calculador.uuid;
+    }
+}
