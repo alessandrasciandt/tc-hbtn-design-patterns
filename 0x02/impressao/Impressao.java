0620e2337c8e0198dd37c7f4bc47fdd6e584a00c
alessandrasciandt <alessandras@ciandt.com>
2022-08-20 23:03:01 -0400

    1. Impressão

--- 0x02/impressao/Impressao.java
+++ 0x02/impressao/Impressao.java
@@ -1,14 +1,14 @@
 public class Impressao {
 
-    private Integer paginasTotais;
-    private Integer paginasColoridas;
+    private int paginasTotais;
+    private int paginasColoridas;
     private boolean ehFrenteVerso;
     private double valorColoridasFrenteVerso;
     private double valorPretoBrancoFrenteVerso;
     private double valorColoridasFrenteApenas;
     private double valorPretoBrancoFrenteApenas;
 
-    public Impressao(Integer paginasTotais, Integer paginasColoridas, boolean ehFrenteVerso, double valorColoridasFrenteVerso, double valorPretoBrancoFrenteVerso, double valorColoridasFrenteApenas, double valorPretoBrancoFrenteApenas) {
+    public Impressao(int paginasTotais, int paginasColoridas, boolean ehFrenteVerso, double valorColoridasFrenteVerso, double valorPretoBrancoFrenteVerso, double valorColoridasFrenteApenas, double valorPretoBrancoFrenteApenas) {
         this.paginasTotais = paginasTotais;
         this.paginasColoridas = paginasColoridas;
         this.ehFrenteVerso = ehFrenteVerso;
